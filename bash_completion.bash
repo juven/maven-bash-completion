38bdbaf4256716875cdc7a59668700bdfd94a59f
Dmitry Jerusalimsky <dmitry_jerusalimsky@cable.comcast.com>
2015-05-07 13:15:14 -0700

    added findbugs

--- bash_completion.bash
+++ bash_completion.bash
@@ -139,6 +139,7 @@ _mvn()
     local plugin_goals_cargo="cargo:start|cargo:run|cargo:stop|cargo:deploy|cargo:undeploy|cargo:help"
     local plugin_goals_checkstyle="checkstyle:checkstyle|checkstyle:check"
     local plugin_goals_cobertura="cobertura:cobertura"
+    local plugin_goals_findbugs="findbugs:findbugs|findbugs:gui|findbugs:help"
     local plugin_goals_dependency="dependency:analyze|dependency:analyze-dep-mgt|dependency:analyze-duplicate|dependency:analyze-only|dependency:analyze-report|dependency:build-classpath|dependency:copy|dependency:copy-dependencies|dependency:get|dependency:go-offline|dependency:help|dependency:list|dependency:list-repositories|dependency:properties|dependency:purge-local-repository|dependency:resolve|dependency:resolve-plugins|dependency:sources|dependency:tree|dependency:unpack|dependency:unpack-dependencies"
     local plugin_goals_deploy="deploy:deploy-file"
     local plugin_goals_ear="ear:ear|ear:generate-application-xml"
